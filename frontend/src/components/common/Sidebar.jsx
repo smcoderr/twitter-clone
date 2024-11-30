3bb17a740b1186fb84f5029d33e228a23d38bcb2
smcoderr <souravoz2018@gmail.com>
2024-11-30 21:43:14 +0530

    small change

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -57,7 +57,7 @@ const Sidebar = () => {
 							className='flex gap-3 items-center hover:bg-stone-900 transition-all rounded-full duration-300 py-2 pl-2 pr-4 max-w-fit cursor-pointer'
 						>
 							<MdHomeFilled className='w-8 h-8' />
-							<span className='text-lg hidden md:block'>Home</span>
+							<span className='text-lg hidden md:block'>Home.</span>
 						</Link>
 					</li>
 					<li className='flex justify-center md:justify-start'>
